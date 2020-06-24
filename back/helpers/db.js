7e4c79892c97330a3d0569ce5c80d9445db9abaf
bspiri <bserdarpiri@gmail.com>
2020-06-24 10:15:15 +0300

    commit for quest050

--- back/helpers/db.js
+++ back/helpers/db.js
@@ -2,7 +2,7 @@ const mysql = require('mysql');
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'PembegotR2+',
+    password: '',
     database: 'my_db'
 });
 module.exports = connection;
