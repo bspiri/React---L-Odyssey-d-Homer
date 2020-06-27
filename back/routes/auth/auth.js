e28e9819a3a92b8c7482715ec766709697fb504f
bspiri <bserdarpiri@gmail.com>
2020-06-27 22:09:27 +0300

    commit for quest040

--- back/routes/auth/auth.js
+++ back/routes/auth/auth.js
@@ -1,13 +1,17 @@
 const express = require("express");
-const app = express();
 const router = express.Router();
+const connection = require("../../helpers/db.js");
 
-app.get("/", (req, res) => {
-    res.send("youhou");
-})
-
-router.post('/signup', function (req, res, next) {
-    res.send('I am in POST signup');
+router.post("/signup", function (req, res, next) {
+    const data = req.body;
+    connection.query("INSERT INTO users SET ?", data, function (err, results, fields) {
+        if (err) {
+            console.log(err);
+            res.status(500).send("Error signing up");
+        } else {
+            res.status(200).send(results);
+        }
+    });
 });
 
 module.exports = router;
